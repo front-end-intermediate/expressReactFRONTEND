c40f7d9e55e94b6b91c81192cb5eb4d94e849b45
Daniel Deverell <daniel.deverell@gmail.com>
2021-12-02 20:28:53 -0600

    done for today

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ import Recipes from "./Recipes";
 import RecipeDetail from "./RecipeDetail";
 import Nav from "./Nav";
 
-import { useFetch } from "./hooks/useFetch";
+import useFetch from "./hooks/useFetch";
 import useToggle from "./hooks/useToggle";
 
 function App() {
-  const { loading, data: recipes, error } = useFetch(`/api/recipes`);
-  // const [loggedin, setLoggedin] = useState(false);
-  const [loggedin, setLoggedin] = useToggle(false);
+  const [recipes, setRecipes] = React.useState([]);
+  const [loggedin, setLoggedin] = useToggle(true);
+  const [loading, setLoading] = useToggle(true);
+  const [error, setError] = React.useState("");
+  const { get, post } = useFetch("/api/recipes");
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  React.useEffect(() => {
+    setLoading(true);
+    get("/api/recipes")
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => setError(error))
+      .finally(setLoading(false));
+  }, []);
+
+  const addRecipe = (recipe) => {
+    post("/api/recipes", recipe).then((data) => {
+      setRecipes([data, ...recipes]);
+    });
+  };
 
   if (loading === true) {
     return <p>Loading</p>;
@@ -26,7 +46,16 @@ function App() {
       <BrowserRouter>
         <Nav loggedin={loggedin} setLoggedin={setLoggedin} />
         <Routes>
-          <Route path="/" element={<Recipes recipes={recipes} />} />
+          <Route
+            path="/"
+            element={
+              <Recipes
+                recipes={recipes}
+                loggedin={loggedin}
+                addRecipe={addRecipe}
+              />
+            }
+          />
           <Route
             path="/:recipeId"
             element={<RecipeDetail recipes={recipes} />}
